49fcdb1ab91f92270003e7d1e30a9f0e6704e6a3
Laxdude52 <everythingcubbies5@gmail.com>
2023-12-26 22:50:25 -0500

    etbeszbeb
    rtbneab

---
+++ environment.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 26 22:26:38 2023
+
+@author: every
+"""
+
+'''
+Necessary Features
+
+Get:
+    Future Model Setting:
+        1. plant levels
+            a. Max ramp up/down
+            b. other associated data
+        2. elec demand pred
+        3. solar output pred
+    Current Logic Setting:
+        1. plant levels
+            a. Max ramp up/down
+            b. other associated data
+        2. real demand:
+            real elec demand - real solar output (future) - other unit output (ex. battery, water)
+ 
+Do:
+    
+
+'''
+
+
+import data as ud
+
